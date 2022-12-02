c15d6d80b16dd2360a0ad2cbb05c93eb3a53b574
Javier-DlaP <javirachi99@hotmail.es>
2022-12-02 19:20:11 +0100

    Day 2 part 1

---
+++ day-2-rock-paper-scissors/solution1.py
@@ -0,0 +1,29 @@
+# Open file separated by '\n' and ' '
+with open('input2.txt', 'r') as file:
+    file = list(map(lambda x: x.split(' '), str(file.read()).split('\n')))
+
+# Remove last '\n'
+file = file[:-1]
+
+# Create the data structure that helps us to store the data
+dict_shape_oponent = {'A': 'Rock', 'B': 'Paper', 'C': 'Scissors'}
+dict_shape_you = {'X': 'Rock', 'Y': 'Paper', 'Z': 'Scissors'}
+dict_points_result = {'Win': 6, 'Tie': 3, 'Lose': 0}
+dict_points_shape = {'Rock': 1, 'Paper': 2, 'Scissors': 3}
+dict_win = {'Rock': 'Scissors', 'Paper': 'Rock', 'Scissors': 'Paper'}
+
+# Function that returns the result of the game
+def result(you, oponent):
+    if you == oponent:
+        return 'Tie'
+    elif dict_win[you] == oponent:
+        return 'Win'
+    else:
+        return 'Lose'
+
+# Calculate the result of every match and the point for the shape played
+result = list(map(lambda x: dict_points_result[result(dict_shape_you[x[1]], dict_shape_oponent[x[0]])] + \
+                            dict_points_shape[dict_shape_you[x[1]]], file))
+
+# Sum all the points
+print(sum(result))
